b1e073347f597756bbfd16c3f432bc568f243a54
GL-Lee <gl_lee@163.com>
2013-08-25 02:15:37 +0800

    成功给img添加hover时间

--- automenu_GL_Lee@163.com/chrome/content/tt.js
+++ automenu_GL_Lee@163.com/chrome/content/tt.js
@@ -1,7 +1,26 @@
-function init(){
-	alert("div");
-var tipHtml = "<box>tip</box>";
-$(tipHtml).appendTo($("window"));
-}
+var am={
+		timer: null,
+		init: function(){
+		var amHtml="<box id='am_tip'>tip</box>";
+		$(amHtml).appendTo($("#main-window"));
+		am.bindEvents();
+	},
+	bindEvents: function(){
+		am.bindImgEvent();
+		am.bindKeyEvent();
+	},
+	bindImgEvent: function(){
+		var doc = $("#content")[0].contentDocument;//获得网页对应的document
+		var $imgs = $("img",$(doc));
+		$imgs.hover(function(){
+			clearTimeout(am.timer);
+			var _this = this;
+			am.timer = setTimeout(function(){
+				$("#am_tip").text(_this.src)
+			},100)},function(){})
+	},
+	bindKeyEvent: function(){
 
-window.onload=init;
+	}
+}
+window.addEventListener("DOMContentLoaded",am.init,false);
