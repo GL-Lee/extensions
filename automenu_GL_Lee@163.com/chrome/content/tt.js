5cee9991c4366f96d9ede7741801fa7a99236811
GL-Lee <gl_lee@163.com>
2013-08-25 21:49:03 +0800

    实现点击触发contextmenu相应操作

--- automenu_GL_Lee@163.com/chrome/content/tt.js
+++ automenu_GL_Lee@163.com/chrome/content/tt.js
@@ -3,11 +3,12 @@ var am={
 	mposition:{left:"0px",top:"0px"},
 	$doc:null,
 	$tip:null,
+	imgTarget:null,
 	intip:0,
 	init: function(){
 		am.$doc = $($("#content")[0].contentDocument);//获得网页对应的document
 		if($("#am_tip",am.$doc).length == 0){
-			var amHtml="<div id='am_tip' style='position:absolute;left:0px;top:0px;border:solid thin;'>tip</div>";
+			var amHtml="<div id='am_tip' style='position:fixed;left:0px;top:0px;border:solid thin;'>tip</div>";
 			am.$tip=$(amHtml).appendTo($("body",am.$doc));
 		}
 		am.bindEvents();
@@ -16,13 +17,15 @@ var am={
 		am.bindMouseEvent();
 		am.bindKeyEvent();
 		am.bindImgEvent();
+		am.bindAmEvent();
 	},
 	bindImgEvent: function(){
 		var $imgs = $("img",am.$doc);
 		$imgs.hover(function(){
 			clearTimeout(am.timer);
 			var _this = this;
-			var _mposition
+			var _mposition;
+			am.imgTarget=this;
 			am.timer = setTimeout(function(){
 				// am.show();
 				var position = am.getPosition(15,-15);
@@ -49,6 +52,12 @@ var am={
 	bindKeyEvent: function(){
 
 	},
+	bindAmEvent: function(){
+		am.$tip.bind("click",function(){
+			am.$tip.hide();
+			am.doCommand("context-saveimage",am.imgTarget);
+		});
+	},
 	getPosition: function(offx,offy,$hoverImg){
 		var position = {left:am.mposition.left+offx,top:am.mposition.top+offy};
 		if($hoverImg && position.left>$hoverImg.css){
@@ -59,7 +68,21 @@ var am={
 	getAbsolutePositon: function(){
 		var position = {};
 		return position;
-	}
+	},
+	doCommand: function(contextMenuitemId,target) {
+	    var fun = document.getElementById(contextMenuitemId).getAttribute("oncommand");
+	    if(!fun){
+	    	var commandId = document.getElementById(id).getAttribute("command");
+	    	fun = document.getElementById(commandId).getAttribute("oncommand");
+	    }
+	    document.popupNode = target;/*
+	    							 * 解决error
+									 * TypeError: aNode is null
+									 * chrome://browser/content/nsContextMenu.js
+									 */
+		gContextMenu = new nsContextMenu(target, gBrowser);
+		eval(fun);
+	},
 }
 window.addEventListener("DOMContentLoaded",am.init,false);
 window.addEventListener("TabSelect",function(){
