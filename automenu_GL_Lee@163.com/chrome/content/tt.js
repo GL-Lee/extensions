8f94b8bf36685fc75e012988815525fa4c34a40d
GL-Lee <gl_lee@163.com>
2013-09-03 23:40:04 +0800

    tooltiptext添加

--- automenu_GL_Lee@163.com/chrome/content/tt.js
+++ automenu_GL_Lee@163.com/chrome/content/tt.js
@@ -5,9 +5,23 @@ var am={
 	$tip:null,
 	imgTarget:null,
 	intip:0,
+	iteminfos:{
+		am_CopyImage:{tooltiptext:"Copy Image",menuitemId:"context-copyimage"},
+		am_CopyLink:{tooltiptext:"Copy Link",menuitemId:"context-copylink"},
+		am_ImageInfo:{tooltiptext:"Image Info",menuitemId:"context-viewimageinfo"},
+		am_SaveImageAs:{tooltiptext:"Save Image As",menuitemId:"context-saveimage"},
+		am_SendImage:{tooltiptext:"Send Image",menuitemId:"context-sendimage"}
+	},
 	init: function(){
-		am.$tip = $("#am_tip");
+		am.$tip = $("#am_wrapper");
+		am.buildPanel();
 		am.bindEvents();
+	},
+	buildPanel: function(){
+		for(var itemid in am.iteminfos){
+			var item = $("<a tooltiptext='"+am.iteminfos[itemid].tooltiptext+"'><image id='"+itemid+"'/></a>");
+			am.$tip.append(item);
+		}
 	},
 	bindEvents: function(){
 		am.bindMouseEvent();
@@ -28,7 +42,7 @@ var am={
 			am.timer = setTimeout(function(){
 				am.imgTarget=target;
 				var position = am.getPosition(15,-15);
-				am.$tip.css({left:position.left,top:position.top}).text(target.src);
+				am.$tip.css({left:position.left,top:position.top});
 				am.$tip.show();
 			},300)
 		}
@@ -51,10 +65,10 @@ var am={
 
 	},
 	bindAmEvent: function(){
-		am.$tip.bind("click",function(){
-			am.$tip.hide();
-			am.doCommand("context-saveimage",am.imgTarget);
-		});
+		$("#am_wrapper").bind("click",function(event){
+			am.doCommand(am.iteminfos[event.target.id].menuitemId,am.imgTarget);
+		})
+		am.$tip.hover(null,function(){$(this).hide()})
 	},
 	getPosition: function(offx,offy,$hoverImg){
 		
